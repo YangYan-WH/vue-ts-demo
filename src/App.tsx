de06ebce904662124a00b8dce83017834bf10cb7
yangyan <15927288912@126.com>
2022-08-06 20:53:32 +0800

    feat: 引入vue-router4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+
+export const App = defineComponent({
+  setup() {
+    return () => <>
+      <div>
+        <RouterView/>
+      </div>
+    </>
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, defineComponent } from 'vue'
+import {App} from './App'
 
-createApp(App).mount('#app')
+const Home = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      123
+    )
+  }
+})
+// const About = { template: '<div>About</div>' }
+
+import {createRouter, createWebHashHistory} from 'vue-router'
+const routes = [
+  { path: '/', component: Home },
+  // { path: '/about', component: About },
+
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
